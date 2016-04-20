f56d3222f158e4db4b47246d8017bb513c93b30b
slgtw <gorlova.k@mail.ru>
2016-04-20 21:33:12 +0300

    clients_email

--- kurs-masterrr/Kurs/addClient.Designer.cs
+++ kurs-masterrr/Kurs/addClient.Designer.cs
@@ -41,13 +41,15 @@
             this.dateTimePicker_Birthday = new System.Windows.Forms.DateTimePicker();
             this.button_add = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox_name
             // 
             this.textBox_name.Location = new System.Drawing.Point(392, 48);
             this.textBox_name.Name = "textBox_name";
-            this.textBox_name.Size = new System.Drawing.Size(100, 20);
+            this.textBox_name.Size = new System.Drawing.Size(200, 20);
             this.textBox_name.TabIndex = 0;
             // 
             // textBox_child
@@ -61,20 +63,21 @@
             // 
             this.textBox_phone.Location = new System.Drawing.Point(392, 255);
             this.textBox_phone.Name = "textBox_phone";
-            this.textBox_phone.Size = new System.Drawing.Size(100, 20);
+            this.textBox_phone.Size = new System.Drawing.Size(200, 20);
             this.textBox_phone.TabIndex = 4;
             // 
             // textBox_dop
             // 
-            this.textBox_dop.Location = new System.Drawing.Point(392, 304);
+            this.textBox_dop.Location = new System.Drawing.Point(392, 357);
+            this.textBox_dop.Multiline = true;
             this.textBox_dop.Name = "textBox_dop";
-            this.textBox_dop.Size = new System.Drawing.Size(100, 20);
+            this.textBox_dop.Size = new System.Drawing.Size(200, 80);
             this.textBox_dop.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(279, 55);
+            this.label1.Location = new System.Drawing.Point(267, 55);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 6;
@@ -83,7 +86,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(279, 104);
+            this.label2.Location = new System.Drawing.Point(264, 111);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 13);
             this.label2.TabIndex = 7;
@@ -92,34 +95,34 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(279, 155);
+            this.label3.Location = new System.Drawing.Point(267, 160);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.Size = new System.Drawing.Size(27, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "пол";
+            this.label3.Text = "Пол";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(279, 211);
+            this.label4.Location = new System.Drawing.Point(267, 211);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.Size = new System.Drawing.Size(98, 13);
             this.label4.TabIndex = 9;
-            this.label4.Text = "дети ";
+            this.label4.Text = "Количество детей";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(279, 262);
+            this.label5.Location = new System.Drawing.Point(267, 258);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.Size = new System.Drawing.Size(114, 13);
             this.label5.TabIndex = 10;
-            this.label5.Text = "телефон";
+            this.label5.Text = "Контактный телефон";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(279, 307);
+            this.label6.Location = new System.Drawing.Point(264, 381);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(71, 13);
             this.label6.TabIndex = 11;
@@ -134,7 +137,7 @@
             // 
             // button_add
             // 
-            this.button_add.Location = new System.Drawing.Point(392, 363);
+            this.button_add.Location = new System.Drawing.Point(401, 470);
             this.button_add.Name = "button_add";
             this.button_add.Size = new System.Drawing.Size(75, 23);
             this.button_add.TabIndex = 13;
@@ -154,11 +157,29 @@
             this.comboBox1.Size = new System.Drawing.Size(134, 21);
             this.comboBox1.TabIndex = 14;
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(267, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Эл. почта";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(392, 302);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 16;
+            // 
             // addClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(642, 415);
+            this.ClientSize = new System.Drawing.Size(734, 530);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button_add);
             this.Controls.Add(this.dateTimePicker_Birthday);
@@ -196,5 +217,7 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker_Birthday;
         private System.Windows.Forms.Button button_add;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
